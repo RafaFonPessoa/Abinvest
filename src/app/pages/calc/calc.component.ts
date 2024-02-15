2aefd88cbb72e8ea652ed5ad2beec0019d245b93
Rafael F Pessoa <112719981+RafaFonPessoa@users.noreply.github.com>
2024-02-15 19:57:47 -0300

    Add files via upload

--- src/app/pages/calc/calc.component.ts
+++ src/app/pages/calc/calc.component.ts
@@ -6,5 +6,45 @@ import { Component } from '@angular/core';
   styleUrl: './calc.component.css'
 })
 export class CalcComponent {
+  preFixado: boolean = false;
+    posFixado: boolean = false;
+    ipcamais: boolean = false;
+    tesouroSelic: boolean = false;
+
+    tipoInvestimentoSelecionado(event: Event) {
+      const target = event.target as HTMLSelectElement;
+      const opcao = target.value;
+      if (opcao === 'tesouro-direto') {
+          this.tesouroSelic = true;
+      } else {
+          this.tesouroSelic = false;
+      }
+  }
+  
+
+  tipoRendimentoSelecionado(event: Event) {
+    const target = event.target as HTMLSelectElement;
+    const opcao = target.value;
+    this.preFixado = false;
+    this.posFixado = false;
+    this.ipcamais = false;
+    this.tesouroSelic = false;
+
+    switch (opcao) {
+        case 'pre':
+            this.preFixado = true;
+            break;
+        case 'pos':
+            this.posFixado = true;
+            break;
+        case 'ipcamais':
+            this.ipcamais = true;
+            break;
+        case 'tesouroSelic':
+            this.tesouroSelic = true;
+            break;
+    }
+}
 
 }
+
